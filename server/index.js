fb5ec5e415e90fb470b55e32de642c6992532923
Rana Shivang Singh <ranashivang567@gmail.com>
2024-10-19 18:31:13 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+// Middleware setup
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// Routes
+app.use('/posts', postRoutes);
+
+// Connection URL
+const CONNECTION_URL = process.env.CONNECTION_URL; // Ensure this is set correctly in your .env file
+const PORT = process.env.PORT || 5000;
+
+// Connect to MongoDB
+mongoose.connect(CONNECTION_URL) // Remove deprecated options
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
